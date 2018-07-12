c2d757d8f493dbbed8d891cde80ee2d08fa74ade
littlewine <amkrasakis@gmail.com>
2018-07-12 19:02:00 +0200

    pickle directory re-org

--- my-code/init.py
+++ my-code/init.py
@@ -1,7 +1,18 @@
+experiment_name = '25similar'
+print "Setting up variables & DB connection for experiment:\n"
+print "*******************\n%s\n*******************"%experiment_name
+
+# point to appropriate DBs, pickle files etc.
+pkl_paths = 'pickles/%s/'%experiment_name
+path_candidate_dict_pkl = pkl_paths+ 'candidate_dict.pickle' # TODO rename that
+path_pubmed_ids_pkl = pkl_paths + 'pubmed_ids.pickle'
+path_base_learners = pkl_paths + 'base_learner_predictions' # TODO create dirs
+
+
 # Shortcuts to connect to database, initialize candidate subclass and return snorkel session
 import os
-# os.environ['SNORKELDB'] = 'postgres:///snorkel'
-os.environ['SNORKELDB'] = 'postgres:///snorkel50similar'
+#TODO: set experiment_name and restructure dir
+os.environ['SNORKELDB'] = 'postgres:///snorkel'+experiment_name
 
 from snorkel import SnorkelSession
 session = SnorkelSession()
@@ -11,4 +22,4 @@ from snorkel.annotations import save_marginals
 from snorkel.models import Candidate, candidate_subclass
 REGULATOR = candidate_subclass('REGULATOR', ['Chemical', 'Gene'])
 
-print "Created snorkel session from ",os.environ['SNORKELDB']
+print "\nSnorkel session connected to: ",os.environ['SNORKELDB']
